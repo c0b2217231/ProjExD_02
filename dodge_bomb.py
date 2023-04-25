74f7ed56a5654a09f598a50e49ba52c1410ea8d2
C0B22172 <c0b2217231@edu.teu.ac.jp>
2023-04-25 15:50:34 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,9 +12,24 @@ delta = {
 
 }
 
+def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外を判定し、真理値をタプルで返す関数
+    引数１：画面SurfaceのRect
+    引数２：こうかとん、または爆弾SurfaceのRect
+    戻り値：横方向、縦方向のはみ出し判定結果（画面内：True/画面外：False)
+    """
+    yoko, tate = True, True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
-    screen = pg.display.set_mode((1600, 900))
+    screen = pg.display.set_mode((1400, 800))
     clock = pg.time.Clock()
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
@@ -25,7 +40,7 @@ def main():
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_img.set_colorkey((0, 0, 0)) #練習1
-    x, y = random.randint(0, 1600), random.randint(0, 900) #練習２
+    x, y = random.randint(0, 1400), random.randint(0, 800) #練習２
     #screen.blit(bb_img, [x, y])#練習２
     vx, vy = +1, +1 #練習３
     bb_rct = bb_img.get_rect() #練習３
@@ -38,15 +53,25 @@ def main():
                 return 0
 
         tmr += 1
+
         key_lst = pg.key.get_pressed()
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
+        if check_bound(screen.get_rect(), kk_rct) != (True, True):
+            for k, mv in delta.items():
+                if key_lst[k]:
+                    kk_rct.move_ip(-mv[0], -mv[1])
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct) #練習4
         #screen.blit(bg_img, [x, y])
         bb_rct.move_ip(vx, vy) #練習３
+        yoko, tate = check_bound(screen.get_rect(), bb_rct)
+        if not yoko: #横方向にはみ出ていたら
+            vx *= -1
+        if not tate: #縦方向に這い出ていたら
+            vy *= -1
         screen.blit(bb_img, bb_rct) #練習3
 
         pg.display.update()
